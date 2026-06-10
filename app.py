83415b31425c4587bee1b8991fdfdcd4fb70eba0
soniajin721 <yeojingg721@gmail.com>
2026-06-10 20:23:49 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,20 @@ st.set_page_config(
     layout="wide"
 )
 
-st.title("문정호 포트폴리오")
+st.image(
+    "https://images.unsplash.com/photo-1511818966892-d7d671e672a2",
+    use_container_width=True
+)
 
-st.write("첫 번째 홈페이지 테스트")
+st.markdown(
+    """
+    <div style="text-align:center;">
+        <h1>MOON JUNGHO</h1>
+        <h3>Architecture Portfolio</h3>
+    </div>
+    """,
+    unsafe_allow_html=True
+)
+
+if st.button("ENTER"):
+    st.switch_page("pages/about.py")
